40664c9b80531fabe2481008d5e3de444f9667c9
unknown <professionaltarget@gmail.com>
2024-07-25 13:51:04 -0400

    Jeb!

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import CompleteModule from "./modules/CompleteModule"
 import RollModule from "./modules/RollModule"
 import AutoRoleModule from "./modules/AutoRoleModule"
 import * as fs from "fs"
+import JebModule from "./modules/JebModule"
 
 async function launchBot() {
     const rollbot = new Rollbot(
@@ -10,7 +11,8 @@ async function launchBot() {
             fs.readFileSync("secrets/discord_client_id").toString("utf-8").trim(), [
         new RollModule(),
         new CompleteModule(),
-        new AutoRoleModule("roles.json")
+        new AutoRoleModule("roles.json"),
+        new JebModule()
     ])
 
     process.on("beforeExit", async () => {
---
+++ src/modules/JebModule.ts
@@ -0,0 +1,127 @@
+import { SlashCommandBuilder, ChatInputCommandInteraction, bold, Client, AttachmentBuilder, Attachment, Embed } from "discord.js"
+import Module, { ModuleCommand } from "./Module"
+import * as fs from "fs"
+import * as sharp from "sharp"
+
+export default class JebModule extends Module {
+    public readonly name = "jeb"
+
+    private readonly boundOnJeb = this.onJeb.bind(this)
+    private readonly boundOnJebUpload = this.onJebUpload.bind(this)
+
+    private jebImage: sharp.Sharp
+
+    public async onStart(client: Client): Promise<void> {
+        this.jebImage = await sharp("data/jeb.png")
+    }
+
+    public async onJeb(interaction: ChatInputCommandInteraction) {
+        try {
+            const messages = await interaction.channel.messages.fetch({
+                limit: 100
+            })
+
+            for (const message of messages.filter(x => x.attachments.size > 0 || x.embeds.length > 0).values()) {
+                for (const imageEmbed of message.embeds.filter(x => x.data.type === "image")) {
+                    const attachment = await this.doJeb(imageEmbed)
+                    if (attachment) {
+                        interaction.reply({files: [attachment]})
+                        return
+                    }
+                }
+                for (const imageAttachment of message.attachments.reverse().values()) {
+                    const attachment = await this.doJeb(imageAttachment)
+                    if (attachment) {
+                        interaction.reply({files: [attachment]})
+                        return
+                    }
+                }
+            }
+
+            interaction.reply({
+                content: "Unable to find jebbable image within the last 100 messages",
+                ephemeral: true
+            })
+
+        } catch (e) {
+            console.error("Something went wrong jebbing", e)
+            interaction.reply({
+                content: "Unexpected error occured whilst jebbing",
+                ephemeral: true
+            })
+        }
+    }
+
+    public async onJebUpload(interaction: ChatInputCommandInteraction) {
+        try {
+            const imageAttachment = interaction.options.getAttachment("image", true)
+            const attachment = await this.doJeb(imageAttachment)
+
+            if (attachment) {
+                interaction.reply({files: [attachment]})
+            }
+            else {
+                interaction.reply({
+                    content: "Unable to jebbify attachment, please make sure it is an image",
+                    ephemeral: true
+                })
+            }
+        }
+        catch (e) {
+            console.error("Something went wrong jebbing", e)
+            interaction.reply({
+                content: "Unexpected error occured whilst jebbing",
+                ephemeral: true
+            })
+        }
+    }
+
+    public getCommands(): ModuleCommand[] {
+        return [
+            {
+                data: new SlashCommandBuilder()
+                    .setName("jeb")
+                    .setDescription("Jeb!"),
+                execute: this.boundOnJeb
+            },
+            {
+                data: new SlashCommandBuilder()
+                .setName("jebupload")
+                .setDescription("Jeb! an uploaded image")
+                .addAttachmentOption(option =>
+                    option
+                    .setName("image")
+                    .setDescription("Image to Jeb!")
+                    .setRequired(true)),
+                execute: this.boundOnJebUpload
+            }
+        ]
+    }
+
+    private async doJeb(attachment: Attachment | Embed): Promise<AttachmentBuilder> {
+        try {
+            if (attachment.url) {
+                const data = await fetch(attachment.url)
+                const dataBuffer = await data.arrayBuffer()
+                const image = await sharp(dataBuffer)
+                const imageMetadata = await image.metadata()
+
+                const jeb = this.jebImage.clone()
+                await jeb.resize(imageMetadata.width, imageMetadata.height, {fit: "inside"})
+                
+                await image.composite([{
+                    input: await jeb.toBuffer(),
+                    gravity: "southwest"
+                }])
+                
+                return new AttachmentBuilder(await image.toBuffer())
+            }
+            else {
+                return null
+            }
+        }
+        catch (e) {
+            return null;
+        }
+    }
+}
